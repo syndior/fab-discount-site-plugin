0fd61f595c874e8d0cce0f2ec6a1e4d3ac9497f6
amuzammil364 <jemniekk@gmail.com>
2021-02-08 17:49:30 +0500

    FD site settings markup

--- includes/admin/class-admin-pages.php
+++ includes/admin/class-admin-pages.php
@@ -30,8 +30,47 @@ class FD_Admin_Pages
                 'menu_title'	=> 'Voucher Settings',
                 'parent_slug'	=> 'fd-site-settings',
             ));
+
+            add_filter('acf/load_field/name=select_static_product_to_show_on_home_page',array($this,'acf_load_products_field_choices'));
+
         }
+
+    }//constructor
+
+    public function acf_load_products_field_choices( $field ) {
+    
+        // reset choices
+    $field['choices'] = array();
+    
+    $args = array(
+        'post_type'=>'product',
+        'numberposts'=>-1
+    );
+
+    //setting deafult values in choices
+    $label = "Select Product To Show";
+    $value = 0;
+    $field['choices'][$value] = $label;
+    
+    // getting all the products except offer
+    $products= get_posts($args);
+    foreach ($products as $key => $product) {
+        $product = wc_get_product($product->ID);
+        
+        if($product->is_type('fd_wc_offer') || $product->is_type('fd_wc_offer_variable')){
+            $label = $product->get_title();
+            $value = $product->get_ID();
+            $field['choices'][$value] = $label; 
+        }//if
+    }//for each
+    
+    // return the field
+    return $field;
+        
     }
-}
+    
+
+
+}//class
 
 new FD_Admin_Pages();
