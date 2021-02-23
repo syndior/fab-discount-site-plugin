202ebeb09f2b702d68ed8e70dd6202979ab6ae0f
amuzammil364 <jemniekk@gmail.com>
2021-02-23 14:46:46 +0500

    2-23-21

--- includes/class-fd-functions.php
+++ includes/class-fd-functions.php
@@ -19,66 +19,94 @@ function fdscf_set_option_for_hero_section()
                 'limit' => 1,
                 'orderby' => 'date',
                 'order' => 'DESC',
-                'type' => 'fd_wc_offer',
-                'type' => 'fd_wc_offer_variable',
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'product_type',
+                        'field'    => 'slug',
+                        'terms'    => array('fd_wc_offer','fd_wc_offer_variable'),
+                    ),
+                ),
+                // 'type' => array('fd_wc_offer','fd_wc_offer_variable'),
             ));
 
             $product = $query->get_products()[0];
-
-            $product_name = $product->get_name();
-            $product_description = $product->get_description();
+            //product initials
             $product_img_url = ( wp_get_attachment_url($product->get_image_id()) ) ? wp_get_attachment_url($product->get_image_id()) : wc_placeholder_img_src() ;
-            $product_url = get_permalink($product->get_id());
+            $product_description = $product->get_description();
             $currency_symbol = get_woocommerce_currency_symbol();
-            $product_actual_price = $product->get_price()?$product->get_price():'';
-            $product_sale_price = $product->get_sale_price()?$product->get_sale_price():'';
-            $saving_percentage = (int)(100-(($product_sale_price/$product_actual_price)*100));
-            $product_sale_price =$currency_symbol.$product_sale_price;
-            $product_actual_price = $currency_symbol.$product_actual_price;
+            $product_url = get_permalink($product->get_id());
+            $product_name = $product->get_name();
+            $sold_qty = $product->get_sold_individually();
+            if($sold_qty == ""){
+                $sold_qty=0;
+            }
+            //checking stock
+            $stock_status = "In Stock";
+            if($product->managing_stock()){
+               $low_stock_amount = (int)wc_get_low_stock_amount( $product );
+               $stock_amount = (int)$product->get_stock_quantity();
+               if($stock_amount>$low_stock_amount){
+                   $stock_status = "In Stock";
+               }else if($stock_amount<=0){
+                   $stock_status = "Out Of Stock";
+               }else{
+                   $stock_status = "Ending Soon";
+               }//status msg if else
+            }//if managing stock
+            //checking stock
 
-            // if($product->is_type('fd_wc_offer')){
-            //     $product_actual_price = $product->get_price()?$product->get_price():'';
-            //     $product_sale_price = $product->get_sale_price()?$product->get_sale_price():'';
-            //     $saving_percentage = (int)(100-(($product_sale_price/$product_actual_price)*100));
-            //     $product_sale_price =$currency_symbol.$product_sale_price;
-            //     $product_actual_price = $currency_symbol.$product_actual_price;
-            // }elseif($product->is_type('fd_wc_offer_variable')) {
-            //     //goal is that we have to filter max saving percentage values
-            //     $product_actual_price_array = array();// (1) in this we will save all all actual prices
-            //     $product_sale_prices_array = array();// (2) in this we will save all all sales prices
-            //     $savings_array = array();// (3) in this we will save all saving percentages
+            if($product->is_type('fd_wc_offer')){
 
-            //     $product_ids = $product->get_children();
-            //     foreach ($product_ids as $key => $product_id) {
-            //         $variab_product = wc_get_product($product_id);
-            //         $regular_price = $variab_product->get_regular_price();
-            //         $sale_price = $variab_product->get_sale_price();
+                $product_actual_price = $product->get_price()?$product->get_price():'';
+                $product_sale_price = $product->get_sale_price()?$product->get_sale_price():'';
+                if($product_actual_price != "" && $product_sale_price!=""){
+                    $saving_percentage = (int)(100-(($product_sale_price/$product_actual_price)*100));    
+                }else {
+                    $saving_percentage = 0;
+                    $product_actual_price = 0;
+                    $product_sale_price = 0;
+
+                }
+                $product_sale_price =$currency_symbol.$product_sale_price;
+                $product_actual_price = $currency_symbol.$product_actual_price;
+
+            }elseif($product->is_type('fd_wc_offer_variable')) {
+                //goal is that we have to filter max saving percentage values
+                $product_actual_price_array = array();// (1) in this we will save all all actual prices
+                $product_sale_prices_array = array();// (2) in this we will save all all sales prices
+                $savings_array = array();// (3) in this we will save all saving percentages
+
+                $product_ids = $product->get_children();
+                foreach ($product_ids as $key => $product_id) {
+                    $variab_product = wc_get_product($product_id);
+                    $regular_price = $variab_product->get_regular_price();
+                    $sale_price = $variab_product->get_sale_price();
                     
-            //         $product_actual_price_array[$key] = $regular_price;//saving actual prices in this array related to (1)
-            //         $product_sale_prices_array[$key] = $regular_price;//saving sales prices in this array related to (2)
+                    $product_actual_price_array[$key] = $regular_price;//saving actual prices in this array related to (1)
+                    $product_sale_prices_array[$key] = $sale_price;//saving sales prices in this array related to (2)
 
-            //         //checking whether both prices are inserted or not
-            //         if($regular_price != "" && $sale_price!=""){
-            //             $savings_array[$key] = (int)(100-(($sale_price/$regular_price)*100));//saving off percentage in this array related to (3)
-            //         }else{
-            //             $savings_array[$key] = 0;//saving off percentage in this array related to (3)     
-            //         }//if els for calculating percentage of saving
+                    //checking whether both prices are inserted or not
+                    if($regular_price != "" && $sale_price!=""){
+                        $savings_array[$key] = (int)(100-(($sale_price/$regular_price)*100));//saving off percentage in this array related to (3)
+                    }else{
+                        $savings_array[$key] = 0;//saving off percentage in this array related to (3)     
+                    }//if els for calculating percentage of saving
                 
-            //     }//foreach for variations
+                }//foreach for variations
 
-            //     //cehcking if saving array has some values
-            //     if(sizeof($savings_array)>0){
-            //         $saving_percentage = max($savings_array)//(3);
-            //         $saving_percentage_key = array_search($saving_percentage,$savings_array)//(3)getting index of that highest savings for sale and actual price  
-            //         $product_sale_price = $product_sale_prices_array[$saving_percentage_key]//(2)getting value of that sale price which is having highest percentage of saving  
-            //         $product_actual_price = $product_actual_price_array[$saving_percentage_key]//(2)getting value of that sale price which is having highest percentage of saving  
+                //cehcking if saving array has some values
+                if(sizeof($savings_array)>0){
+                    $saving_percentage = (int)max($savings_array);//(3);
+                    $saving_percentage_key = array_search($saving_percentage,$savings_array);//(3)getting index of that highest savings for sale and actual price  
+                    $product_sale_price = $currency_symbol.$product_sale_prices_array[$saving_percentage_key];//(2)getting value of that sale price which is having highest percentage of saving  
+                    $product_actual_price = $currency_symbol.$product_actual_price_array[$saving_percentage_key];//(2)getting value of that sale price which is having highest percentage of saving  
                     
-            //     }else {
-            //         $saving_percentage=0;
-            //         $product_sale_price='';
-            //         $product_actual_price='';
-            //     }
-            // }//main else to process fd_wc_offer_variable
+                }else {
+                    $saving_percentage=0;
+                    $product_sale_price='';
+                    $product_actual_price='';
+                }
+            }//main else to process fd_wc_offer_variable
             
     
         } //if featured product
@@ -87,17 +115,88 @@ function fdscf_set_option_for_hero_section()
 
             $product = wc_get_product($static_product_id);
                 
-            $product_name = $product->get_name();
-            $product_description = $product->get_description();
+            //product initials
             $product_img_url = ( wp_get_attachment_url($product->get_image_id()) ) ? wp_get_attachment_url($product->get_image_id()) : wc_placeholder_img_src() ;
-            $product_actual_price = $product->get_price()?$product->get_price():10;
-            $product_sale_price = $product->get_sale_price()?$product->get_sale_price():10;
-            $saving_percentage = ($product_sale_price/$product_actual_price)*100;
-
+            $product_description = $product->get_description();
             $currency_symbol = get_woocommerce_currency_symbol();
-            $product_sale_price =$currency_symbol.$product_sale_price;
-            $product_actual_price = $currency_symbol.$product_actual_price;
             $product_url = get_permalink($product->get_id());
+            $product_name = $product->get_name();
+
+            $sold_qty = $product->get_sold_individually();
+            if($sold_qty == ""){
+                $sold_qty=0;
+            }
+
+            //checking stock
+            $stock_status = "In Stock";
+            if($product->managing_stock()){
+               $low_stock_amount = (int)wc_get_low_stock_amount( $product );
+               $stock_amount = (int)$product->get_stock_quantity();
+               if($stock_amount>$low_stock_amount){
+                   $stock_status = "In Stock";
+               }else if($stock_amount<=0){
+                   $stock_status = "Out Of Stock";
+               }else{
+                   $stock_status = "Ending Soon";
+               }//status msg if else
+            }//if managing stock
+            //checking stock
+
+
+            if($product->is_type('fd_wc_offer')){
+
+                $product_actual_price = $product->get_price()?$product->get_price():'';
+                $product_sale_price = $product->get_sale_price()?$product->get_sale_price():'';
+                if($product_actual_price != "" && $product_sale_price!=""){
+                    $saving_percentage = (int)(100-(($product_sale_price/$product_actual_price)*100));    
+                }else {
+                    $saving_percentage = 0;
+                    $product_actual_price = 0;
+                    $product_sale_price = 0;
+                }
+                $product_sale_price =$currency_symbol.$product_sale_price;
+                $product_actual_price = $currency_symbol.$product_actual_price;
+
+            }elseif($product->is_type('fd_wc_offer_variable')) {
+                //goal is that we have to filter max saving percentage values
+                $product_actual_price_array = array();// (1) in this we will save all all actual prices
+                $product_sale_prices_array = array();// (2) in this we will save all all sales prices
+                $savings_array = array();// (3) in this we will save all saving percentages
+
+                $product_ids = $product->get_children();
+                foreach ($product_ids as $key => $product_id) {
+                    $variab_product = wc_get_product($product_id);
+                    $regular_price = $variab_product->get_regular_price();
+                    $sale_price = $variab_product->get_sale_price();
+                    
+                    $product_actual_price_array[$key] = $regular_price;//saving actual prices in this array related to (1)
+                    $product_sale_prices_array[$key] = $sale_price;//saving sales prices in this array related to (2)
+
+                    //checking whether both prices are inserted or not
+                    if($regular_price != "" && $sale_price!=""){
+                        $savings_array[$key] = (int)(100-(($sale_price/$regular_price)*100));//saving off percentage in this array related to (3)
+                    }else{
+                        $savings_array[$key] = 0;//saving off percentage in this array related to (3)     
+                    }//if els for calculating percentage of saving
+                
+                }//foreach for variations
+
+                //cehcking if saving array has some values
+                if(sizeof($savings_array)>0){
+                    $saving_percentage = (int)max($savings_array);//(3);
+                    $saving_percentage_key = array_search($saving_percentage,$savings_array);//(3)getting index of that highest savings for sale and actual price  
+                    $product_sale_price = $currency_symbol.$product_sale_prices_array[$saving_percentage_key];//(2)getting value of that sale price which is having highest percentage of saving  
+                    $product_actual_price = $currency_symbol.$product_actual_price_array[$saving_percentage_key];//(2)getting value of that sale price which is having highest percentage of saving  
+                    
+                }else {
+                    $saving_percentage=0;
+                    $product_sale_price='';
+                    $product_actual_price='';
+                }
+            }//main else to process fd_wc_offer_variable
+
+
+
         } //if static product
 
         update_option('fdscf_hero_product_url', $product_url);
@@ -107,6 +206,8 @@ function fdscf_set_option_for_hero_section()
         update_option('fdscf_hero_product_image', $product_img_url);
         update_option('fdscf_hero_product_title', $product_name);
         update_option('fdscf_hero_product_description', $product_description);
+        update_option('fdscf_hero_product_sold_count', $sold_qty);
+        update_option('fdscf_hero_product_stock_status', $stock_status);
 
     } //if section enabled
 
@@ -147,8 +248,6 @@ function fd_product_sold(){
     // get_sold_individually()
 }
 
-// fd_wc_offer_variable
-// fd_wc_offer
 function fd_product_saving_percentage(){
     global $post;
     $product = wc_get_product($post->ID);
@@ -171,9 +270,8 @@ function fd_product_saving_percentage(){
                 $savings_array[$key] = (int)(100-(($sale_price/$regular_price)*100));     
             }else{
                 $savings_array[$key] = 0;     
-            }//if els for calculating percentage of saving
-        
-        }//foreach for variations
+            }
+        }
 
         if(sizeof($savings_array)>0){
             $savings = max($savings_array);
@@ -184,3 +282,5 @@ function fd_product_saving_percentage(){
     }
     echo $savings;
 }
+
+
